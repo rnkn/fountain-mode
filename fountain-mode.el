21df459e7c351b513941b1b7c7a72a0a20f62db6
Paul Rankin <paul@tilk.co>
2014-02-26 18:53:34 +1000

    Changed version number

--- fountain-mode.el
+++ fountain-mode.el
@@ -1,7 +1,7 @@
 ;;; fountain-mode.el --- Major mode for editing Fountain-formatted text files
 
 ;; Author: Paul Rankin <paul@tilk.co>
-;; Version: 0.9.0
+;; Version: 0.9.1
 ;; Keywords: wp
 ;; Package-Requires: ((s "1.9.0"))
 ;; URL: http://github.com/rnkn/fountain-mode/
