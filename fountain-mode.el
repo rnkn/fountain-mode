95f18e8868dfcc7205f9416761421ed763398b7f
Paul Rankin <paul@tilk.co>
2014-03-18 13:56:45 +1000

    Added version number

--- fountain-mode.el
+++ fountain-mode.el
@@ -4,6 +4,7 @@
 
 ;; Author: Paul Rankin <paul@tilk.co>
 ;; Keywords: wp
+;; Version: 0.9.4
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
