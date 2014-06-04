1c54ebcd182cc4b848fe106d20aa4f64585eeda8
Paul Rankin <paul@tilk.co>
2014-06-04 13:30:18 +1000

    Changed version number

--- fountain-mode.el
+++ fountain-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Paul Rankin <paul@tilk.co>
 ;; Keywords: wp
-;; Version: 1.2.0
+;; Version: 1.2.1
 ;; Package-Requires: ((s "1.9.0"))
 ;; URL: https://github.com/rnkn/fountain-mode/
 
@@ -131,7 +131,7 @@
   :link '(url-link "http://github.com/rnkn/fountain-mode/"))
 
 (defconst fountain-version
-  "1.2.0")
+  "1.2.1")
 
 ;;; Obsolete Aliases ===================================================
 
