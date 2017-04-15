f4aa022c6030ea330e402b3e7be73d0b88cea743
Paul W. Rankin <rnkn@rnkn.xyz>
2017-04-15 23:19:11 +1000

    Function fountain-element

--- fountain-mode.el
+++ fountain-mode.el
@@ -1132,6 +1132,26 @@ comments."
                         (fountain-match-note)))
                (looking-at fountain-action-regexp))))))
 
+(defun fountain-element ()
+  "Return element at point as a symbol"
+  (save-excursion
+    (save-restriction
+      (widen)
+      (forward-line 0)
+      (cond
+       ((fountain-match-metadata) 'metadata)
+       ((fountain-match-section-heading) 'section-heading)
+       ((fountain-match-scene-heading) 'scene-heading)
+       ((fountain-match-character) 'character)
+       ((fountain-match-dialog) 'dialog)
+       ((fountain-match-paren) 'paren)
+       ((fountain-match-trans) trans)
+       ((fountain-match-center) 'center)
+       ((fountain-match-synopsis) 'synopsis)
+       ((fountain-match-note) 'note)
+       ((fountain-match-page-break) 'page-break)
+       (t 'action)))))
+
 
 ;;; Emacs Bugs
 
