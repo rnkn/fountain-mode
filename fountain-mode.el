4910a50f1636e4886a75b090125ccc3bf483c6f4
Paul Rankin <hello@paulwrankin.com>
2017-07-23 23:49:07 +1000

    Replace .editorconfig with Local Variables

--- fountain-mode.el
+++ fountain-mode.el
@@ -4318,4 +4318,12 @@ fountain-hide-ELEMENT is non-nil, adds fountain-ELEMENT to
   (fountain-outline-hide-level fountain-outline-startup-level t))
 
 (provide 'fountain-mode)
+
+;; Local Variables:
+;; coding: utf-8
+;; fill-column: 80
+;; indent-tabs-mode: nil
+;; require-final-newline: t
+;; End:
+
 ;;; fountain-mode.el ends here
