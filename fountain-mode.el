578dfd4643527a051f0b575eb05df4501c1706a5
Paul Rankin <hello@paulwrankin.com>
2017-12-14 23:15:20 +1000

    Fix include & page-break matching as action

--- fountain-mode.el
+++ fountain-mode.el
@@ -1144,6 +1144,8 @@ Assumes that all other element matching has been done."
           (and (not (or (and (bolp) (eolp))
                         (fountain-match-section-heading)
                         (fountain-match-scene-heading)
+                        (fountain-match-include)
+                        (fountain-match-page-break)
                         (fountain-match-character)
                         (fountain-match-dialog)
                         (fountain-match-paren)
