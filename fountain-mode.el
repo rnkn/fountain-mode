6f526b880a19387bf8adff21103a921ff9ddfaca
Paul W. Rankin <rnkn@rnkn.xyz>
2020-10-14 18:52:03 +1000

    Fix character extension completion with only opening paren

--- fountain-mode.el
+++ fountain-mode.el
@@ -553,7 +553,7 @@ Requires `fountain-match-metadata' for `bobp'.")
           "\\|"
           "\\(?2:[^<>\n[:lower:]]*?[[:upper:]]+[^<>\n[:lower:]]*?\\)"
           "\\)"
-          "\\(?3:[\s\t]*\\(?4:(\\).*?)\\)*?"
+          "\\(?3:[\s\t]*\\(?4:\(\\).*?\\)*?"
           "\\(?5:[\s\t]*^\\)?"
           "[\s\t]*$")
   "Regular expression for matching character names.
