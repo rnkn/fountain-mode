6fe9ac479a09ece4d589c4f43952fdd753778d8d
Paul W. Rankin <pwr@bydasein.com>
2021-04-06 00:00:45 +1000

    Bump version to 3.5

--- fountain-mode.el
+++ fountain-mode.el
@@ -6,7 +6,7 @@
 
 ;; Author: Paul W. Rankin <pwr@bydasein.com>
 ;; Keywords: wp, text
-;; Version: 3.4.1
+;; Version: 3.5.0
 ;; Package-Requires: ((emacs "24.4") (seq "2.20"))
 ;; URL: https://github.com/rnkn/fountain-mode
 
