94744c2d2e42133f3e30ef8149b0373bd0e2ce0e
Paul W. Rankin <hello@paulwrankin.com>
2022-12-21 11:08:38 +1000

    Bump version to 3.6.2

--- fountain-mode.el
+++ fountain-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Paul W. Rankin <hello@paulwrankin.com>
 ;; Keywords: wp, text
-;; Version: 3.6.1
+;; Version: 3.6.2
 ;; Package-Requires: ((emacs "24.4") (seq "2.20"))
 ;; URL: https://github.com/rnkn/fountain-mode
 
