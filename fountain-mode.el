db9d5cfa869569bb027fb82cdc971e94b0b7c410
Paul W. Rankin <rnkn@rnkn.xyz>
2023-09-30 21:30:15 +1000

    Remove xflag 3

--- fountain-mode.el
+++ fountain-mode.el
@@ -2934,7 +2934,6 @@ Options are: bold, double-space, underline."
 (defvar fountain-export-troff-macro
   "\
 .\\\" -*- mode: nroff -*-
-.do xflag 3
 .if !rPS .nr PS 12
 .ps \\n[PS]
 .if !rVS .nr VS 12
